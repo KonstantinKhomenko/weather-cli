e3ee4a6a2f77369974548539a90e179986a8a208
Konstantin Khomenko <k.khomenko@upstars.com>
2023-08-30 21:23:33 +0300

    help

--- services/log.service.js
+++ services/log.service.js
@@ -1,3 +1,19 @@
-const printError = (error) => {
-    console.log(`Error ${error}`);
+import chalk from 'chalk'
+import dedent from 'dedent-js'
+
+export const printError = (error) => {
+    console.log(`${chalk.bgRed(' Error ')} ${error}`);
+}
+
+export const printSuccess = (message) => {
+    console.log(`${chalk.bgGreen(' Error ')} ${message}`);
+}
+
+export const printHelp = () => {
+    console.log(
+        dedent`${chalk.bgCyan.blackBright.bold(' Help ')}
+        Без параметров - вывод погоды
+        -s [City] для установки города
+        -h для вывода помощи
+        -t [Api_key] для сохранения токена`)
 }
